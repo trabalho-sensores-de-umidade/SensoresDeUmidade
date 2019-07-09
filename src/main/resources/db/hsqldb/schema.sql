c093eac609b5f6a26164797c77d4983d40f8c09d
leticia1404 <let.demoura@gmail.com>
2019-07-09 15:45:04 -0300

    #5

--- src/main/resources/db/hsqldb/schema.sql
+++ src/main/resources/db/hsqldb/schema.sql
@@ -1,32 +1,21 @@
-DROP TABLE vet_specialties IF EXISTS;
-DROP TABLE vets IF EXISTS;
-DROP TABLE specialties IF EXISTS;
-DROP TABLE visits IF EXISTS;
-DROP TABLE pets IF EXISTS;
-DROP TABLE types IF EXISTS;
+DROP TABLE sensor IF EXISTS;
 DROP TABLE owners IF EXISTS;
-DROP TABLE products IF EXISTS;
+DROP TABLE plants IF EXISTS;
+DROP TABLE types IF EXISTS;
+DROP TABLE reading IF EXISTS;
 
+CREATE TABLE sensor (
+  id         INTEGER IDENTITY PRIMARY KEY,
+  name VARCHAR(30),
+  );
+CREATE INDEX sensor_name ON sensor (name);
 
-CREATE TABLE vets (
+CREATE TABLE owners (
   id         INTEGER IDENTITY PRIMARY KEY,
   first_name VARCHAR(30),
-  last_name  VARCHAR(30)
+  last_name  VARCHAR_IGNORECASE(30),
 );
-CREATE INDEX vets_last_name ON vets (last_name);
-
-CREATE TABLE specialties (
-  id   INTEGER IDENTITY PRIMARY KEY,
-  name VARCHAR(80)
-);
-CREATE INDEX specialties_name ON specialties (name);
-
-CREATE TABLE vet_specialties (
-  vet_id       INTEGER NOT NULL,
-  specialty_id INTEGER NOT NULL
-);
-ALTER TABLE vet_specialties ADD CONSTRAINT fk_vet_specialties_vets FOREIGN KEY (vet_id) REFERENCES vets (id);
-ALTER TABLE vet_specialties ADD CONSTRAINT fk_vet_specialties_specialties FOREIGN KEY (specialty_id) REFERENCES specialties (id);
+CREATE INDEX owners_last_name ON owners (last_name);
 
 CREATE TABLE types (
   id   INTEGER IDENTITY PRIMARY KEY,
@@ -34,42 +23,29 @@ CREATE TABLE types (
 );
 CREATE INDEX types_name ON types (name);
 
-CREATE TABLE owners (
-  id         INTEGER IDENTITY PRIMARY KEY,
-  first_name VARCHAR(30),
-  last_name  VARCHAR_IGNORECASE(30),
-  address    VARCHAR(255),
-  city       VARCHAR(80),
-  telephone  VARCHAR(20)
+CREATE TABLE plants (
+  id                 INTEGER IDENTITY PRIMARY KEY,
+  name               VARCHAR(30),
+  moisture_minimum   INTEGER NOT NULL,
+  moisture_maximum   INTEGER NOT NULL,
+  type_id            INTEGER NOT NULL,
+  owner_id           INTEGER NOT NULL
 );
-CREATE INDEX owners_last_name ON owners (last_name);
+ALTER TABLE plants ADD CONSTRAINT fk_plants_owners FOREIGN KEY (owner_id) REFERENCES owners (id);
+ALTER TABLE plants ADD CONSTRAINT fk_plants_types FOREIGN KEY (type_id) REFERENCES types (id);
+CREATE INDEX plants_name ON plants (name);
 
-CREATE TABLE pets (
-  id         INTEGER IDENTITY PRIMARY KEY,
-  name       VARCHAR(30),
-  birth_date DATE,
-  type_id    INTEGER NOT NULL,
-  owner_id   INTEGER NOT NULL
+CREATE TABLE reading (
+  id               INTEGER IDENTITY PRIMARY KEY,
+  plant_id         INTEGER NOT NULL,
+  sensor_id        INTEGER NOT NULL,
+  reading_date     DATE,
+  reading_hour     TIME,
+  moisture_current INTEGER
 );
-ALTER TABLE pets ADD CONSTRAINT fk_pets_owners FOREIGN KEY (owner_id) REFERENCES owners (id);
-ALTER TABLE pets ADD CONSTRAINT fk_pets_types FOREIGN KEY (type_id) REFERENCES types (id);
-CREATE INDEX pets_name ON pets (name);
-
-CREATE TABLE visits (
-  id          INTEGER IDENTITY PRIMARY KEY,
-  pet_id      INTEGER NOT NULL,
-  visit_date  DATE,
-  description VARCHAR(255)
-);
-ALTER TABLE visits ADD CONSTRAINT fk_visits_pets FOREIGN KEY (pet_id) REFERENCES pets (id);
-CREATE INDEX visits_pet_id ON visits (pet_id);
-
-CREATE TABLE products (
-  id   INTEGER IDENTITY PRIMARY KEY,
-  name VARCHAR(80),
-  description CLOB(5K)  
-);
-CREATE INDEX products_name ON products (name);
+ALTER TABLE reading ADD CONSTRAINT fk_reading_plants FOREIGN KEY (plant_id) REFERENCES plants (id);
+ALTER TABLE reading ADD CONSTRAINT fk_reading_sensor FOREIGN KEY (sensor_id) REFERENCES sensor (id);
+CREATE INDEX reading_plant_id ON reading (plant_id);
 
 
 
