8b9712b4c805af84f177555a925a54002fd97e06
Leticia <let.demoura@gmail.com>
2019-07-12 00:57:13 -0300

    #6

--- src/main/java/org/springframework/samples/petclinic/sensor/HumiditySensorRepository.java
+++ src/main/java/org/springframework/samples/petclinic/sensor/HumiditySensorRepository.java
@@ -1,9 +1,15 @@
 package org.springframework.samples.petclinic.sensor;
 
+import java.util.Collection;
+
 import org.springframework.data.repository.Repository;
+import org.springframework.data.repository.query.Param;
 
 public interface HumiditySensorRepository extends Repository<HumiditySensor, Integer>{
 
+	Collection<HumiditySensor> findAll();
+	HumiditySensor findById(@Param("id") Integer id);
+	
 	
     /**
      * Save an {@link HumiditySensor} to the data store, either inserting or updating it.
