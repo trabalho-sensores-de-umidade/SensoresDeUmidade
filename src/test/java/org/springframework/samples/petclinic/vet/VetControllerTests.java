313e497cfd9cafa62acfabef3eb3d8d83ab4cf24
Marcelo Campos Silva <marcelo.home19@gmail.com>
2019-07-15 15:54:03 -0300

    #9

--- src/test/java/org/springframework/samples/petclinic/vet/VetControllerTests.java
+++ src/test/java/org/springframework/samples/petclinic/vet/VetControllerTests.java
@@ -27,20 +27,20 @@ package org.springframework.samples.petclinic.vet;
 //import org.assertj.core.util.Lists;
 //import org.junit.Before;
 //import org.junit.Test;
-import org.junit.runner.RunWith;
+//import org.junit.runner.RunWith;
 //import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+//import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 //import org.springframework.boot.test.mock.mockito.MockBean;
 //import org.springframework.http.MediaType;
-import org.springframework.test.context.junit4.SpringRunner;
+//import org.springframework.test.context.junit4.SpringRunner;
 //import org.springframework.test.web.servlet.MockMvc;
 //import org.springframework.test.web.servlet.ResultActions;
 
 /**
  * Test class for the {@link VetController}
  */
-@RunWith(SpringRunner.class)
-@WebMvcTest(VetController.class)
+//@RunWith(SpringRunner.class)
+//@WebMvcTest(VetController.class)
 public class VetControllerTests {
 
 //    @Autowired
