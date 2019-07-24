882797346ec064fe0ac9b954be180232fe96aee2
Marcelo <marcelo.home19@gmail.com>
2019-07-24 14:08:19 -0300

    #28

--- src/test/java/org/springframework/samples/petclinic/service/EmailControllerTest.java
+++ src/test/java/org/springframework/samples/petclinic/service/EmailControllerTest.java
@@ -1,27 +1,22 @@
 package org.springframework.samples.petclinic.service;
 
-import org.assertj.core.api.Assertions;
 import org.junit.Test;
 import org.junit.runner.RunWith;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
-import org.springframework.boot.test.web.client.TestRestTemplate;
 import org.springframework.boot.web.server.LocalServerPort;
-import org.springframework.http.ResponseEntity;
 import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest(webEnvironment = WebEnvironment.RANDOM_PORT)
 public class EmailControllerTest {
 
-	@Autowired
-	private TestRestTemplate restTemplate;
+//	@Autowired
+//	private TestRestTemplate restTemplate;
 
 	@LocalServerPort
 	private int port;	
 	
-	private EmailController email;
 	
 	@Test
     public void testSendEmail() throws Exception {
